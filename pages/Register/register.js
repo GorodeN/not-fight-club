b23751c573e83d9c892a2c0391a1aeab89538642
GorodeN <rshamaluk@gmail.com>
2025-08-23 04:02:09 +0300

    feat: implement layouts

---
+++ pages/Register/register.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const characterName = document.getElementById("character-name");
+  const registerBtn = document.querySelector(".register__button");
+
+  registerBtn.addEventListener("click", () => {
+    if (!characterName.value || characterName.value.length < 1) {
+      characterName.value = "Guest";
+    }
+
+    const character = {
+      name: characterName.value,
+      avatar: "DEFAULT",
+      wins: 0,
+      loses: 0,
+    };
+
+    try {
+      localStorage.setItem("characterInfo", JSON.stringify(character));
+      if (characterName.value) location.href = "/pages/Main/main.html";
+    } catch (e) {}
+  });
+});
